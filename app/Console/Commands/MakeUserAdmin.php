a26e71c57b96225a6d0a5853155dcb53142db989
ozodbekrajabboyev <ozodbek2625@egmail.com>
2025-05-16 11:20:10 +0500

    CustomCommandForCreatingAdmin

---
+++ app/Console/Commands/MakeUserAdmin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-user-admin {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+
+        $user = User::where('email', $email)->first();
+
+        if (! $user) {
+            $this->error("❌ User with email {$email} not found.");
+            return;
+        }
+
+        $user->is_admin = true;
+        $user->save();
+
+        $this->info("✅ {$user->name} is now an admin.");
+    }
+}
