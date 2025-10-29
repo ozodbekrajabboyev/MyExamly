0e4b233ffe926512858a656d2041ac5eb62baff7
Norbek Hamidov <norbekhamidov@kali.Ucell.net>
2025-10-29 18:57:32 +0500

    Expired & Expiring notification logic is complete with (SCHEDULER FOR EVERY 6 HOURS)

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule as ScheduleAlias;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+\Illuminate\Support\Facades\Schedule::command('certificates:check-expiry')->everySixHours();
